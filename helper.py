73bdc0791a9101f77b44d5a096d79ad7426890f0
Fouz <Fouz6@outlook.com>
2021-06-16 20:24:58 +0500

    input added

--- helper.py
+++ helper.py
@@ -1,39 +1,32 @@
-linearEquations = [              # 3 EQUATIONS WITH 3 CO EFFICIENTS
-    [1.0, 3.0, 1.0, 10.0],          # x = 1, y = 2, z = 3
-    [1.00, -2.00, -1.0, -6.0],
-    [2.0, 1.0, 2.0, 10.0]
-]
-# linearEquations = [              # 2 EQUATIONS WITH 2 CO EFFICIENTS
-#     [1.00,1.00,2.0],             # x = 1, y = 1
-#     [-1.0,2.0,1.0],
-#     ]
-# linearEquations = [              # 3 EQUATIONS WITH 3 CO EFFICIENTS
-#     [1.0,10.0,-1.0,10.0],        # x = 1, y = 1, z = 1
-#     [10.00,1.00,1.0,12.0],
-#     [1.0,-2.0,10.0,9.0]
-#     ]
+a = int(input('number of equations: '))
 
-# linearEquations = [              # 4 EQUATIONS WITH 4 CO EFFICIENTS
-#     [1.00,1.00,1.0,1.0,4.0],     # w = 1, x = 1, y = 1, z = 1
-#     [1.0,2.0,-1.0,1.0,3.0],
-#     [3.0,-2.0,5.0,4.0,10.0],
-#     [1.0,2.0,3.0,4.0,10.0]
-#     ]
+linearEquations = []
+
+for i in range(a):
+    equation = []
+    for j in range(a+1):
+        var = chr(123-a+j)
+        if(j < a):
+            b = int(input('Enter coefficient of '+var+': '))
+        else:
+            b = int(input('Enter value: '))
+        equation.append(b)
+    linearEquations.append(equation)
+# print(linearEquations)
 # ------------------------------------------------------------------------------------------------------------------
 
-# IF 1ST ELEMENT OF 1ST EQUATION IS NOT EQUAL TO 1 THEN CONVERTING IT.....
-if(linearEquations[0][0] != 1):
-    x = linearEquations[0][0]
-    # LOOP FOR DIVIDING EVERY ELEMENT OF 1ST LIST BY 1ST ELEMENT OF THAT LIST
-    for i in range(len(linearEquations[0])):
-        y = linearEquations[0][i]
-        linearEquations[0][i] = y/x
 
-numberOfEq = len(linearEquations)  # CHECKING NUMBERSS OF EQUATION
+numberOfEq = len(linearEquations)  # CHECKING NUMBERS OF EQUATION
 numberOfCoef = len(linearEquations[0])
 firstLoop = 0
 while(firstLoop < numberOfEq-1):
-
+    # IF 1ST ELEMENT OF 1ST EQUATION IS NOT EQUAL TO 1 THEN CONVERTING IT.....
+    if(linearEquations[firstLoop][firstLoop] > 1 and linearEquations[firstLoop][firstLoop] < -1):
+        x = linearEquations[firstLoop][firstLoop]
+        # LOOP FOR DIVIDING EVERY ELEMENT OF 1ST LIST BY 1ST ELEMENT OF THAT LIST
+        for i in range(len(linearEquations[firstLoop])):
+            y = linearEquations[firstLoop][i]
+            linearEquations[firstLoop][i] = y/x
     # print('-----------first---------------')
     # print(firstLoop)
     middleLoop = firstLoop + 1
@@ -45,7 +38,6 @@ while(firstLoop < numberOfEq-1):
         InnerLoop = 0
 
         # VARIABLES FOR ROW MULTIPLICATION OPERATION
-        c = linearEquations[firstLoop][firstLoop]
         d = linearEquations[middleLoop][firstLoop]
 
         while (InnerLoop < len(linearEquations[0])):
@@ -53,15 +45,15 @@ while(firstLoop < numberOfEq-1):
             a = linearEquations[middleLoop][InnerLoop]  # THIS IS EQUAL TO D
             b = linearEquations[firstLoop][InnerLoop]   # THIS IS EQUAL TO C
             # linearEquations[middleLoop][InnerLoop] = linearEquations[middleLoop][InnerLoop]-linearEquations[firstLoop][InnerLoop]
-            linearEquations[middleLoop][InnerLoop] = a-((d*b)/c)
+            linearEquations[middleLoop][InnerLoop] = a-(d*b)
             # print(linearEquations[firstLoop][InnerLoop])
             # print(InnerLoop)
 
             InnerLoop = InnerLoop + 1
         middleLoop = middleLoop + 1
     firstLoop = firstLoop + 1
-
-# print(linearEquations)
+# for i in range(len(linearEquations)):
+#     print(linearEquations[i])
 # --------------------------------------------------------------------------------------------------------------------------------------
 # IF 1ST ELEMENT OF 1ST EQUATION IS NOT EQUAL TO 1 THEN CONVERTING IT.....
 if(linearEquations[numberOfEq-1][numberOfCoef-2] != 1):
@@ -89,7 +81,6 @@ while(index > firstLoop):
         numberOfCoef = len(linearEquations[0])
 
         # VARIABLES FOR ROW MULTIPLICATION OPERATION
-        c = linearEquations[index][index]
         d = linearEquations[middleLoop][index]
 
         while (numberOfCoef-1 >= InnerLoop):
@@ -98,27 +89,14 @@ while(index > firstLoop):
             a = linearEquations[middleLoop][numberOfCoef - 1]
             # THIS IS EQUAL TO C IN FIRST LOOP
             b = linearEquations[index][numberOfCoef - 1]
-            # linearEquations[middleLoopLimit][InnerLoop] = linearEquations[middleLoopLimit][InnerLoop]-linearEquations[firstLoop][InnerLoop]
-            result = a-((d*b)/c)
+
+            result = a-(d*b)
             linearEquations[middleLoop][numberOfCoef -
-                                        1] = result  # a-((d*b)/c)
-            # print(linearEquations[firstLoop][InnerLoop])
-            # print(numberOfCoef-1)
+                                        1] = result  # a-(d*b)
 
             numberOfCoef = numberOfCoef - 1
         middleLoop = middleLoop - 1
     index = index - 1
 
-# print(linearEquations)
-
-for i in range(1, len(linearEquations)-1, 1):
-    var = linearEquations[i][i]
-    if(var != 1):
-        value = linearEquations[i][len(linearEquations[0])-1]
-        linearEquations[i][i] = var/var
-        linearEquations[i][len(linearEquations[0])-1] = value/var
-
-print(linearEquations)
-
-# for i in range(len(linearEquations)):
-#     print('value is: '+str(linearEquations[i][4]/linearEquations[i][i]))
+for i in range(len(linearEquations)):
+    print(chr(ord('z') + 1 - numberOfEq+i)+'='+str(linearEquations[i][i])+'\n')
